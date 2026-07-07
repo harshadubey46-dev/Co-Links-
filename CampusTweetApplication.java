bd1a8e1ecb126edfe9226bac7d4f6e79e857ca76
Harsha Dubey <harshadubey46@gmail.com>
2026-07-07 09:10:38 +0530

    Add files via upload

---
+++ CampusTweetApplication.java
@@ -0,0 +1,11 @@
+
+package com.campustweet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusTweetApplication{
+ public static void main(String[] args){
+  SpringApplication.run(CampusTweetApplication.class,args);
+ }
+}
